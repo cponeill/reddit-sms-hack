dc4f283d9373f5d4adb50a2a2d3beb7ebb54dbed
cponeill <caseyoneill78@hotmail.com>
2013-11-10 11:35:26 -0800

    node

---
+++ app.js
@@ -0,0 +1,47 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
+var restler = require('restler');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.all('/', function(request, response) {
+  restler.get('http://reddit.com/.json').on('complete', function(reddit){
+  	var titles = "";
+  	for (var i = 0; i < 5; i++) {
+  	  titles += reddit.data.children[i].data.title;
+  	}
+  	response.send(titles);
+  });
+});
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
